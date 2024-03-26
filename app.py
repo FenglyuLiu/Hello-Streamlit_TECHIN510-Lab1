bac46e910b0188691a81b79485367366b28f8657
Felicity <88125716+FenglyuLiu@users.noreply.github.com>
2024-03-26 14:44:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.header('Hello')
 st.header('I am :blue[Felicity] :sunglasses:')
 
-st.caption('a UX Researcher with a background in HCI and UX Design.')
-st.caption('My passion lies in merging design thinking and research insights to craft meaningful and impactful solutions.')
+st.subheader('a UX Researcher with a background in HCI and UX Design.')
+
+st.subheader('My passion lies in merging design thinking and research insights to craft meaningful and impactful solutions.')
 
 ##st.image('sunrise.jpg', caption='Sunrise by the mountains')
