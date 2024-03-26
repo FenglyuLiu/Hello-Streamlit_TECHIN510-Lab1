2fd435c4ee6a5bedae16c09351742b45a43aab7c
GitHub/VSCode/PlatformIO Lab <liufenglyu@gmail.com>
2024-03-26 15:36:19 -0700

    pic

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ st.subheader('a UX Researcher with a background in HCI and UX Design.')
 
 st.subheader('My passion lies in merging design thinking and research insights to craft meaningful and impactful solutions.')
 
-image_path = '/Users/liufenglyu/Downloads/MSTI/2024Spring/510/TECHIN510-Lab1/pictures/Felicity.jpg'
+image_path = '/Users/liufenglyu/Downloads/MSTI/2024Spring/510/TECHIN510-Lab1/pictures/Felicity.png'
 st.image(image_path, caption='In Yunnan, China')
 
