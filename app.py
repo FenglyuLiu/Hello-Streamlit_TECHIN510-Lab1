931a66a879165bf22e0501915014b4fc94a876f5
Felicity <88125716+FenglyuLiu@users.noreply.github.com>
2024-03-26 14:30:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title('Hello')
-st.title('I am :blue[Felicity] :sunglasses: , a UX Researcher with a background in HCI and UX Design.', divider='rainbow')
+st.title('I am :blue[Felicity] :sunglasses:', divider='rainbow')
 
-st.subheader('My passion lies in merging design thinking and research insights to craft meaningful and impactful solutions.  🌈 I am dedicated to addressing real-world challenges through innovative technology and design.')
+st.subheader('My passion lies in merging design thinking and research insights to craft meaningful and impactful solutions. I am dedicated to addressing real-world challenges through innovative technology and design.')
