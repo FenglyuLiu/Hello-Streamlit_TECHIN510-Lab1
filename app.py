1a651fb32a5109851e1982a804e3a4d587d2d0ed
Felicity <88125716+FenglyuLiu@users.noreply.github.com>
2024-03-26 14:32:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title('Hello')
-st.title('I am :blue[Felicity] :sunglasses:', divider='rainbow')
+st.title('I am :blue[Felicity] :sunglasses:')
 
 ## st.subheader('My passion lies in merging design thinking and research insights to craft meaningful and impactful solutions.')
